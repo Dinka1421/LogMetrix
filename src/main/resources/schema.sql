92e87e91d8311098316475980075375aa68e1a32
josko-heh <josko.jurcevic@2e-systems.com>
2023-06-15 19:21:03 +0200

    id sequences

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
+drop sequence if exists users_seq;
+create sequence users_seq start with 1 increment by 5;
 create table if not exists users (
-    id       identity,
+    id bigint default users_seq.nextval primary key,
     username varchar(100) not null,
     password varchar(250) not null,
     first_name varchar(250) not null,
@@ -8,16 +10,17 @@ create table if not exists users (
 
 drop table if exists filters cascade; 
 drop sequence if exists filters_seq;
-create sequence filters_seq start with 1 increment by 50;
+create sequence filters_seq start with 1 increment by 5;
 create table filters (
+  id bigint default filters_seq.nextval primary key,
   end_date date, 
   start_date date, 
-  id bigint not null, 
   client varchar(255) check (client in ('BWA','CTN','LX')), 
   environment varchar(255) check (environment in ('DEV','TEST','INT','PROD')), 
-  log varchar(255) check (log in ('SYSTEM','DEBUG','ERROR','DATA')), 
-  primary key (id)
+  log varchar(255) check (log in ('SYSTEM','DEBUG','ERROR','DATA'))
 );
+
+
 create table if not exists authority (
     id   identity,
     name varchar(100) not null
